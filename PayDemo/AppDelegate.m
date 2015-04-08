541c9455e21e33bf2bd5d73866dda9de7dd9f4c1
William <william@photofast.com>
2015-04-08 16:09:40 +0800

    test 1

--- PayDemo/AppDelegate.m
+++ PayDemo/AppDelegate.m
@@ -17,6 +17,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    NSLog(@"william test");
+    
     return YES;
 }
 
